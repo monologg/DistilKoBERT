bab23465491982496928bbeddeafad5b3f6480dd
monologg <adieujw@gmail.com>
2020-02-13 02:33:52 +0900

    Update

--- tokenization_kobert.py
+++ tokenization_kobert.py
@@ -184,7 +184,7 @@ class KoBertTokenizer(PreTrainedTokenizer):
 
     def convert_tokens_to_string(self, tokens):
         """Converts a sequence of tokens (strings for sub-words) in a single string."""
-        out_string = ''.join(tokens).replace(SPIECE_UNDERLINE, " ").strip()
+        out_string = "".join(tokens).replace(SPIECE_UNDERLINE, " ").strip()
         return out_string
 
     def build_inputs_with_special_tokens(self, token_ids_0, token_ids_1=None):
